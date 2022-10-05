1f778da1ae586a35c9dbfde84696a7f947866d03
Jim Raine <jimraine@users.noreply.github.com>
2022-10-05 10:30:18 -0600

    Update Q1305.sql
    Reformatted SQL to be easier to read
    Changed `name = 'powershell.exe'` to 'path like 'powershell.exe'` as the name field is not available in WIN_PROCESS_EVENTS and thru a SQL FORMATTING ERROR upon execution
    
    Signed-off-by: Jim Raine <jimraine@users.noreply.github.com>

--- live_queries/Windows_Q1000-1999/Q1305.sql
+++ live_queries/Windows_Q1000-1999/Q1305.sql
@@ -2,5 +2,10 @@
 -- Overview: SQL command to detect 'base64' as a string in a powershell command
 -- Outcome: Detect instances of the string 'base64' used in powershell commands
 -- Attribution: Jordan Durham @EclecticIQ
-select * from win_process_events
-where name = "powershell.exe" and cmdline like '%base64%'
+select
+  *
+from
+  win_process_events
+where
+  name = "powershell.exe"
+  and cmdline like '%base64%'
