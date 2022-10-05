b32a72ea010942524cd73b204f257a91f6cc5c87
Jim Raine <jimraine@users.noreply.github.com>
2022-10-05 10:33:52 -0600

    Update Q1302.sql
    Reformatted SQL to make easier to read
    
    Signed-off-by: Jim Raine <jimraine@users.noreply.github.com>

--- live_queries/Windows_Q1000-1999/Q1302.sql
+++ live_queries/Windows_Q1000-1999/Q1302.sql
@@ -2,8 +2,12 @@
 -- Overview: SQL query to decode powershell encoded commands (base64)
 -- Outcome: Detect Powershell commands ran with -encodedcommand flag and decode the command passed 
 -- Attribution: Jordan Durham @EclecticIQ
-select cmdline,
-regex_match(cmdline,'-e\w*\s(.*?)$', 1) as encoded_command,
-from_base64(regex_match(cmdline,'-e\w*\s(.*?)$', 1)) as decoded_command
-from win_process_events
-where path like '%powershell%' and encoded_command is not null
+select
+  cmdline,
+  regex_match (cmdline, '-e\w*\s(.*?)$', 1) as encoded_command,
+  from_base64 (regex_match (cmdline, '-e\w*\s(.*?)$', 1)) as decoded_command
+from
+  win_process_events
+where
+  path like '%powershell%'
+  and encoded_command is not null
