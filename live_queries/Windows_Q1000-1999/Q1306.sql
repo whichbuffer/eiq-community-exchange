dcde2cd693466845caa52657c9fd08d386565c4d
Jim Raine <jimraine@users.noreply.github.com>
2022-10-05 10:31:00 -0600

    Update Q1306.sql
    Reformatted SQL to be easier to read
    
    Signed-off-by: Jim Raine <jimraine@users.noreply.github.com>

--- live_queries/Windows_Q1000-1999/Q1306.sql
+++ live_queries/Windows_Q1000-1999/Q1306.sql
@@ -1,5 +1,14 @@
 -- Comments are ignored. Copy and paste into EcelcticIQ Live Query.
 -- Overview: SQL command to detect Mimikatz modules used in command lines 
 -- Attribution: Jordan Durham @EclecticIQ
-select cmdline from win_process_events
-where cmdline like '%sekurlsa::%' or cmdline like '%kerberos::%' or cmdline like '%crypto::%' or cmdline like '%lsadump::%' or cmdline like '%vault::%' or cmdline like '%token::%'
+select
+  cmdline
+from
+  win_process_events
+where
+  cmdline like '%sekurlsa::%'
+  or cmdline like '%kerberos::%'
+  or cmdline like '%crypto::%'
+  or cmdline like '%lsadump::%'
+  or cmdline like '%vault::%'
+  or cmdline like '%token::%'
