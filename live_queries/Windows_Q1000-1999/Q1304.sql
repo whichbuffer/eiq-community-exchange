1ef801fbadc2187f641dd743d59b49f0cc65ebfb
Jim Raine <jimraine@users.noreply.github.com>
2022-10-05 10:28:48 -0600

    Update Q1304.sql
    Reformatted SQL to be easier to read
    Changed `name = 'powershell.exe'` to 'path like 'powershell.exe'` as the name field is not available in WIN_PROCESS_EVENTS and thru a SQL FORMATTING ERROR upon execution
    
    Signed-off-by: Jim Raine <jimraine@users.noreply.github.com>

--- live_queries/Windows_Q1000-1999/Q1304.sql
+++ live_queries/Windows_Q1000-1999/Q1304.sql
@@ -3,6 +3,17 @@
 -- Outcome: Detect suspicious cmdlets ran in powershell
 -- Option: Add, change, remove cmdline like options as required 
 -- Attribution: Jordan Durham @EclecticIQ
-select *
-from win_process_events
-where name = 'powershell.exe' and (cmdline like '%-nop%' or cmdline like '%-noni%' or cmdline like '%invoke-expression%' or cmdline like '%iex%' or cmdline like '%.downloadstring%' or cmdline like '%downloadfile%')
+select
+  *
+from
+  win_process_events
+where
+  path like 'powershell.exe'
+  and (
+    cmdline like '%-nop%'
+    or cmdline like '%-noni%'
+    or cmdline like '%invoke-expression%'
+    or cmdline like '%iex%'
+    or cmdline like '%.downloadstring%'
+    or cmdline like '%downloadfile%'
+  )
