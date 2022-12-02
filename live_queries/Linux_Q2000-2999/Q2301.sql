92840cf0e6722af822fbd9a93a908787376b68e4
Jim Raine <jimraine@users.noreply.github.com>
2022-12-01 17:30:58 -0700

    Update Q2301.sql
    Signed-off-by: Jim Raine <jimraine@users.noreply.github.com>

--- live_queries/Linux_Q2000-2999/Q2301.sql
+++ live_queries/Linux_Q2000-2999/Q2301.sql
@@ -2,15 +2,19 @@
 -- Overview: SQL command to detect potential SSH tunnels (ssh sourcing web port traffic) and pulling back username used and source host
 -- Options: Adjust ports checked for as required in where statement
 -- Attribution: Jordan Durham @EclecticIQ
-select socket_events.pid,
-socket_events.path,
-socket_events.remote_address,
-socket_events.remote_port ,
-processes.cmdline,
-logged_in_users.host,
-regex_match(processes.cmdline,'sshd:\s.*?\@(.*?)$', 1) as terminalused,
-regex_match(processes.cmdline,'sshd:\s(.*?)@', 1) as username
-from socket_events
-inner join processes on socket_events.pid = processes.pid
-inner join logged_in_users on terminalused = logged_in_users.tty
-where socket_events.path like '%ssh%' and socket_events.remote_port in ('80','443','8080')
+select
+    socket_events.pid,
+    socket_events.path,
+    socket_events.remote_address,
+    socket_events.remote_port,
+    processes.cmdline,
+    logged_in_users.host,
+    regex_match (processes.cmdline, 'sshd:\s.*?\@(.*?)$', 1) as terminalused,
+    regex_match (processes.cmdline, 'sshd:\s(.*?)@', 1) as username
+from
+    socket_events
+    inner join processes on socket_events.pid = processes.pid
+    inner join logged_in_users on terminalused = logged_in_users.tty
+where
+    socket_events.path like '%ssh%'
+    and socket_events.remote_port in ('80', '443', '8080')
